ca6ab761156537909c401c5136d25e09d508b3ec
Remingusu <leween.m33@gmail.com>
2023-01-23 08:11:07 +0100

    act 3 finis + exomaison5

--- 3 - Représentation des données - types construits/ExoMaisons/ExoMaisons5.py
+++ 3 - Représentation des données - types construits/ExoMaisons/ExoMaisons5.py
@@ -8,7 +8,7 @@ Exemple :
 Entrée : L=[3,2,7,1,3,3,4,5,6,3,7,2] et 3
 Sortie : Il y a 7 occurrences supérieures à l'occurrence 5 dans la liste
 """
-
+"""
 L = [3, 2, 7, 1, 3, 3, 4, 5, 6, 3, 7, 2]
 nbr = int(input("Entrez un nombre pour compter son nombre d'occurrence: "))
 i = 0
@@ -16,7 +16,7 @@ i = 0
 for value in L:
     if value > nbr:
         i += 1
-print(f"Il y a {i} occurrences supérieures au nombre {nbr} dans la liste.")
+print(f"Il y a {i} occurrences supérieures au nombre {nbr} dans la liste.")"""
 
 """Code2** :
 Comptage du nombre d'occurrences (nombres égaux) dans une liste triée
