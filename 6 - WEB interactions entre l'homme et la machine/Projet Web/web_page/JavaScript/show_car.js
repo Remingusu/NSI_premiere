267a97a3dafcdc86ee1eccb5b5ee0f7ab7152796
Remingusu <leween.m33@gmail.com>
2023-02-06 10:18:42 +0100

    circuit_list.html finished

---
+++ 6 - WEB interactions entre l'homme et la machine/Projet Web/web_page/JavaScript/show_car.js
@@ -0,0 +1,25 @@
+function voitureF1() {
+    var f1 = document.getElementById("voitureF1");
+    document.getElementById("bahreïnF1").style.height = "50px";
+    document.getElementById("djeddahF1").style.height = "50px";
+    document.getElementById("melbourneF1").style.height = "50px";		
+    document.getElementById("bakuF1").style.height = "50px";
+    document.getElementById("miamiF1").style.height = "50px";
+    document.getElementById("imolaF1").style.height = "50px";
+    document.getElementById("monacoF1").style.height = "50px";
+    document.getElementById("espagneF1").style.height = "50px";
+    document.getElementById("villeneuveF1").style.height = "50px";
+    document.getElementById("redbullF1").style.height = "50px";
+    document.getElementById("silverstoneF1").style.height = "50px";
+    document.getElementById("hongrieF1").style.height = "50px";
+    document.getElementById("spaF1").style.height = "50px";
+    document.getElementById("zandvoortF1").style.height = "50px";
+    document.getElementById("monzaF1").style.height = "50px";
+    document.getElementById("singapourF1").style.height = "50px";
+    document.getElementById("suzukaF1").style.height = "50px";
+    document.getElementById("qatarF1").style.height = "75px";
+    document.getElementById("austinF1").style.height = "50px";
+    document.getElementById("mexicoF1").style.height = "50px";
+    document.getElementById("brésilF1").style.height = "65px";
+    document.getElementById("abudhabiF1").style.height = "50px";
+    }
