6f834910521a81c334d328b6ae91c378a5c1da56
LeweenH <lewee.m33@gmail.com>
2023-04-06 09:45:20 +0200

    css only

--- 7 - Traitement de données en tables/Projet Traitement de données en tables/project/site/map.py
+++ 7 - Traitement de données en tables/Projet Traitement de données en tables/project/site/map.py
@@ -4,25 +4,30 @@ from webbrowser import open as open_web
 
 
 def fabricationCarte(fichier):
+    cc = 0
+    sm = 0
     carte1 = folium.Map(location=[47.471646, -0.551846], zoom_start=13)  # carte centrée sur le centre d'Angers
-    carte2 = folium.Map(location=[47.471646, -0.551846], zoom_strat=13)
+    carte2 = folium.Map(location=[47.471646, -0.551846], zoom_start=14)
     with open(fichier, 'r') as file:
         for ligne in file:
             nom, departement, lat, long, type_mag = ligne.strip().split(";")
             if departement == 'Maine et Loire' and type_mag == 'Centre commercial':
-                folium.Marker([float(lat), 0 - float(long)], popup="Centre commercial", icon=folium.Icon(color="blue")).add_to(carte1)
+                cc += 1
+                folium.Marker([float(lat), 0 - float(long)], popup=nom + "- Centre commercial", icon=folium.Icon(color="blue")).add_to(carte1)
             if departement == 'Maine et Loire' and type_mag == 'Supermarché':
-                folium.Marker([float(lat), 0 - float(long)], popup="Supermarché", icon=folium.Icon(color="red")).add_to(carte2)
+                sm += 1
+                folium.Marker([float(lat), 0 - float(long)], popup=nom + "- Supermarché", icon=folium.Icon(color="red")).add_to(carte2)
         carte1.save('centre_commerciaux.html')
         carte2.save('supermarkets.html')
+    return cc, sm
 
 
-fabricationCarte("magasin_angers.csv")
+cc, sm = fabricationCarte("magasin_angers.csv")
 
 
-def page_html(file):
+def page_html(file, cc, sm):
     with open(file, 'w', encoding='utf-8') as html_file:
-        html_file.write(f"""
+        html_file.write("""
 <!DOCTYPE html>
 <head>
     <link rel="stylesheet" href="style.css">
@@ -34,16 +39,16 @@ def page_html(file):
         <div class="title">Magasins Angevins</div>
     </header>
     <main>
+        <p id="text">Nombre de Centres commerciaux: """ + str(cc) + """<br>Nombre de Supermarchés: """ + str(sm) + """</p>
         <iframe id="map" src="centre_commerciaux.html" frameborder="0" height="700" width="1000"></iframe><br>
-        <button id="btn-1">BT1</button>
-        <button id="btn-2">BT2</button>
+        <button id="btn-1">Centres Commerciaux</button>
+        <button id="btn-2">Supermarchés</button>
     </main>
     <script src="id.js"></script>
 </body>
 </html>
-
 """)
 
 
-page_html('index.html')
+page_html('index.html', cc, sm)
 open_web(getcwd()+"/index.html")
