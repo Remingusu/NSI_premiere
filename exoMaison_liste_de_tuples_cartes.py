28a5eec24f891be89d45b8adf1f94bda845fafd6
Remingusu <leween.m33@gmail.com>
2023-05-19 19:30:44 +0200

    files update

---
+++ exoMaison_liste_de_tuples_cartes.py
@@ -0,0 +1,15 @@
+﻿""" Code1* : 0,5 point
+Créez un code, avec une boucle, permettant d'afficher toutes les cartes de coeur :
+1 de ♡
+...
+Valet de ♡
+Reine de ♡
+Roi de ♡
+Aide : pour afficher le symbole du coeur : print(chr(0x2661))
+"""
+
+
+""" Code2** : 0,5 point
+Créez un code, avec une boucle, permettant de créer la liste de tuples suivante :.
+[(1,'♡'),(2,'♡'),(3,'♡'),(4,'♡'),(5,'♡'),(6,'♡'),(7,'♡'),(8,'♡'),(9,'♡'),('Valet','♡'),('Reine','♡'),('Roi','♡')]
+"""
