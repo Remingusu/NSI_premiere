9d0a1c13b1d697fffe349714e29b98dc29269331
LeweenH <lewee.m33@gmail.com>
2023-05-22 11:08:07 +0200

    exomaison

---
+++ 3 - Représentation des données - types construits/ExoMaisons/ExoMaison11.py
@@ -0,0 +1,24 @@
+﻿""" Code1* : 0,5 point
+Créez un code permettant d'insérer le dernier élément d'une liste au début de la liste
+Exemple :
+    Entrée : L=[2,3,4,1]
+    Sortie : L=[1,2,3,4]
+"""
+L = [2, 3, 4, 1]
+L.insert(0, L[-1])
+L.pop(-1)
+print(L)
+
+""" Code2** : 0,5 point
+Créez un code permettant de permuter le dernier élément d'une liste avec le premier élément de la liste
+Exemple :
+    Entrée : L=[2,3,4,1]
+    Sortie : L=[1,3,4,2]
+"""
+L = [2, 3, 4, 1]
+val_temp = L[-1]
+L.insert(-1, L[0])
+L.pop(0)
+L.insert(0, val_temp)
+L.pop(-1)
+print(L)
